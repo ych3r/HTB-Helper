4e0442637a9e7fd45bd2c14ec14e70ef9ce6a4b6
ych3r <liuyc26@gmail.com>
2025-01-21 21:43:31 -0500

    add web module

--- htb-helper.sh
+++ htb-helper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source ./scan.lib
+source ./web.lib
 
 if [ $# -lt 1 ]; then
     echo "Usage: $0 <ip>"
@@ -11,9 +12,31 @@ TARGET=$1
 
 echo "Target IP: $TARGET"
 echo
+
+# Basic Scanning
+echo "## Port Scanning"
 nmap_tcp_scan
 echo
 nmap_udp_scan
 echo
-nmap_tcp_all_port_scan
+read -p "-> Full port scan (y/n):" choice
+case "$choice" in
+    y|Y ) nmap_tcp_all_port_scan;;
+    n|N ) ;;
+    * ) echo "invalid";;
+esac
 echo
+# TODO: Service Enum
+# Web Enum
+read -p "-> Web Enum (y/n): " choice
+echo
+case "$choice" in
+    y|Y )
+        echo "## Web Enum"
+        read -p "-> Port: " port
+        web_whatweb
+        web_robots
+        ;;
+    n|N ) ;;
+    * ) echo "invalid";;
+esac
